fa20958509909752a0c41ac0b5ea680f10f49b9a
Mohamed Qassem <moqassemae@gmail.com>
2023-03-04 16:47:47 +0000

    add honeycom custom span

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -2,18 +2,29 @@
 from aws_xray_sdk.core import xray_recorder
 # -------------------------------------------------------
 
+#--------------Honeycomb Global-Config-------------------
+from opentelemetry import trace
+tracer = trace.get_tracer("home.activities")
+#--------------------------------------------------------
+
 from datetime import datetime, timedelta, timezone
 class HomeActivities:
-   # -------------CloudWatch IN-LINE Config-------------------
   def run(Logger):
-    Logger.info('Hello CloudWatch! from  /api/activities/home')
+    #--------------Honeycomb IN-LINE Config-------------------
+    with tracer.start_as_current_span("HomeActivities-MockData"): 
+      span = trace.get_current_span()
+      now = datetime.now(timezone.utc).astimezone()
+      span.set_attribute("home-activities-app.now", now.isoformat())
+    #----------------------------------------------------------
+
+    # -------------CloudWatch IN-LINE Config------------------- 
+      Logger.info('Hello CloudWatch! from  /api/activities/home')
     # ---------------------------------------------------------
 
     # -------------AWS-X Ray IN-LINE Config-------------------
     subsegment = xray_recorder.begin_subsegment('home_activities')
     # ---------------------------------------------------------
   
-    now = datetime.now(timezone.utc).astimezone()
     results = [{
       'uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
       'handle':  'Andrew Brown',
@@ -60,4 +71,8 @@ class HomeActivities:
     subsegment.put_metadata('key', dict, 'namespace')
     xray_recorder.end_subsegment()
     # ---------------------------------------------------------
+
+    #--------------Honeycomb IN-LINE Config-------------------
+    span.set_attribute("home-activities-app.result_length", len(results))
+    #----------------------------------------------------------
     return results
